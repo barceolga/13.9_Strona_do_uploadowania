31e53706b895cbf04cbb8f8d861288c64dbec9e5
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-05-19 16:41:24 +0200

    Update content for server.js

--- modules/server.js
+++ modules/server.js
@@ -28,7 +28,7 @@ function start() {
   }
 
   http.createServer(onRequest).listen(9000);
-  console.log("Server is operating on localhost:9000.")
+  console.log("Server is operating on localhost:9000.".blue)
   console.log("The server has been started!".green);
 }
 
