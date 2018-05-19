93d85834b163a76971eeb1d2a4764ef74385bc15
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-05-19 11:36:06 +0200

    Modify content for handlers.js

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,13 +1,25 @@
+var formidable = require('formidable');
+
 exports.upload = function(request, response) {
     console.log("Starting to operate the upload's request.");
-    response.write("Starting to upload!");
-    response.end();
+    var form = new formidable.IncomingForm();
+    form.parse(request, function(error, fields, files) {
+      fs.renameSync(files.upload.path, "test.png");
+      response.writeHead(200, {"Content-Type": "text/html"});
+      response.write("Received image:<br>");
+      response.write("<img src='/show' />");
+      response.end();
+    });
 }
 
 exports.welcome = function(request, response) {
     console.log("Starting to operate the welcome's request.");
-    response.write("Welcome to the home page!");
-    response.end();
+    fs.readFile('templates/start.html', function(err, html) {
+      response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+      response.write(html);
+      response.end();
+    })
+
 }
 
 exports.error = function(request, response) {
