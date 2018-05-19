4d2f0a156ebee9c439a5920e94746f0893594c46
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-05-19 16:52:20 +0200

    Update  all methods for handlers.js.

--- modules/handlers.js
+++ modules/handlers.js
@@ -3,28 +3,36 @@ var fs = require('fs');
 
 exports.upload = function(request, response) {
     console.log("Starting to operate the upload's request.");
+
     var form = new formidable.IncomingForm();
-    form.parse(request, function(error, fields, files) {
-      fs.renameSync(files.upload.path, "test.png");
-      response.writeHead(200, {"Content-Type": "text/html"});
-      response.write("Received image:<br>");
-      response.write("<img src='/show' />");
-      response.end();
+
+    form.parse(request, function(err, fields, files) {
+        if (err) throw err;
+
+        fs.renameSync(files.upload.path, "test.png");
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("Received image:<br>");
+        response.write("<img src='/show' />");
+        response.end();
     });
 }
 
 exports.welcome = function(request, response) {
     console.log("Starting to operate the welcome's request.");
-    fs.readFile('templates/start.html', function(err, html) {
-      response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-      response.write(html);
-      response.end();
+        fs.readFile('templates/start.html', function(err, html) {
+        if (err) throw err;
+
+        response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+        response.write(html);
+        response.end();
     })
 
 }
 
 exports.show = function(request, response) {
-    fs.readFile("test.png", "binary", function(error, file) {
+    fs.readFile("test.png", "binary", function(err, file) {
+        if (err) throw err;
+
         response.writeHead(200, {"Content-Type": "image/png"});
         response.write(file, "binary");
         response.end();
@@ -32,7 +40,7 @@ exports.show = function(request, response) {
 }
 
 exports.error = function(request, response) {
-    console.log("I don't have any clue what to do...");
-    response.write("404 :(");
-    response.end();
+        console.log("I don't have any clue what to do...");
+        response.write("404 :(");
+        response.end();
 }
